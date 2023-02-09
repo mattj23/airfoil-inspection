eb76455e87f07e402c895e3b74f4266513e7c2af
Matt Jarvis <mattj23@gmail.com>
2023-02-08 23:24:05 -0500

    basic intersections

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::fmt::Debug;
-use nalgebra::{distance, Point3, Scalar, Vector2, Vector3};
-use ncollide2d::shape::{ConvexPolygon, Polyline};
-use ncollide2d::na::Point2;
+use nalgebra::{Point, Point3};
+use ncollide2d::na::{Isometry2, Point2, Vector2};
+use ncollide2d::shape::{ConvexPolygon, Polyline, Segment};
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 use std::fs;
+use ncollide2d::query::{Ray, RayCast};
+use std::f64::consts::PI;
 
 fn main() {
     let data = fs::read_to_string("/home/matt/working/airfoil/data/E-E.json")
@@ -28,8 +29,53 @@ fn main() {
     let (i0, i1) = farthest_pair(&hull);
     let p0 = &hull.points()[i0];
     let p1 = &hull.points()[i1];
-    println!("Max distance ({:?}, {:?}): {}", p0, p1, dist_2d(p0, p1));
+    let rough_chord = Ray::new(*p0, p1 - p0);
+    let rotate_90 = Isometry2::rotation(PI / 2.0);
+    let rough_n = rotate_90 * rough_chord.dir.normalize();
 
+    println!("Max distance ({:?}, {:?}): {}", p0, p1, dist_2d(p0, p1));
+    for i in 1..10 {
+        let ray = Ray::new(rough_chord.point_at(i as f64 / 10.0), rough_n);
+        let ints = intersections(&ray, &poly);
+        print!("Ray {}: ", i);
+        for p in ints {
+            print!("{:?} ", p);
+        }
+        print!("\n");
+    }
+
+
+}
+
+fn intersection_param(a0: &Point2<f64>, ad: &Vector2<f64>, b0: &Point2<f64>, bd: &Vector2<f64>) -> Option<f64> {
+    let det: f64 = bd.x * ad.y - bd.y * ad.x;
+    if det.abs() < 1e-6 {
+        return Option::None;
+    }
+
+    let dx = b0.x - a0.x;
+    let dy = b0.y - a0.y;
+
+    Some((dy * bd.x - dx * bd.y) / det)
+}
+
+fn intersections(ray: &Ray<f64>, poly: &Polyline<f64>) -> Vec<Point2<f64>> {
+    let mut results: Vec<Point2<f64>> = Vec::new();
+    for (i, edge) in poly.edges().iter().enumerate() {
+        let p0 = &poly.points()[edge.indices.coords[0]];
+        let p1 = &poly.points()[edge.indices.coords[1]];
+        let d = p1 - p0;
+        let param = intersection_param(&p0, &d, &ray.origin, &ray.dir);
+        if param.is_some() {
+            let f = param.unwrap();
+            if 0.0 <= f  && f <= 1.0 {
+                results.push(ray.origin + ray.dir * param.unwrap());
+            }
+        }
+    }
+    results.sort_by(|a, b| a[0].partial_cmp(&b[0]).unwrap());
+    remove_adjacent_duplicates(&mut results);
+    results
 }
 
 fn farthest_pair(hull: &ConvexPolygon<f64>) -> (usize, usize) {
